9e525083c286009f512c916b61f99ee2f8c3fb34
Wajid <errormrcoder@example.com>
2025-08-17 08:53:35 +0500

    Add scan page to scan qr code.

--- Src/QR_Handler/Core/urls.py
+++ Src/QR_Handler/Core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import Home
+from .views import Home, Scan
 urlpatterns = [
-    path("", Home, name="home")
+    path("", Home, name="home"),
+    path("scan", Scan,name="scan")
 ]
