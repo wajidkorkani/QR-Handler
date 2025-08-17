a6c4379e1fe221b02ac9552713fb6f5205e6266f
Wajid <errormrcoder@example.com>
2025-08-17 23:51:30 +0500

    Apply settings to show media files.

--- Src/QR_Handler/QR_Handler/urls.py
+++ Src/QR_Handler/QR_Handler/urls.py
@@ -12,3 +12,5 @@ urlpatterns = [
 # Serve static files in development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:  # only for development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
