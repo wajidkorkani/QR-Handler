f03a883cae6a1785ecc82ab3c353006e7516b4af
Wajid <errormrcoder@example.com>
2025-08-18 08:13:32 +0500

    Create urls or apis for defined views.

--- Src/QR_Handler/Core/urls.py
+++ Src/QR_Handler/Core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Home, Scan, ScanResult
+from .views import Home, Scan, ScanResult, GenerateQRCode, DisplayQRImage
 urlpatterns = [
     path("", Home, name="home"),
     path("scan", Scan,name="scan"),
-    path("qr-results", ScanResult, name="scan_result")
+    path("qr-results", ScanResult, name="scan_result"),
+    path("generate-code", GenerateQRCode, name="generateQRCode"),
+    path('display-qr-image', DisplayQRImage, name="displayQRImage")
 ]
