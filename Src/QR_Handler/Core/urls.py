a4df47759b55c9e5d6781d9c88b4cda2fa5e0cd1
Wajid <errormrcoder@example.com>
2025-08-18 08:42:28 +0500

    Define url/api for live qr code scan.

--- Src/QR_Handler/Core/urls.py
+++ Src/QR_Handler/Core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Home, Scan, ScanResult, GenerateQRCode, DisplayQRImage
+from .views import Home, Scan, ScanResult, GenerateQRCode, DisplayQRImage, LiveScan
 urlpatterns = [
     path("", Home, name="home"),
     path("scan", Scan,name="scan"),
     path("qr-results", ScanResult, name="scan_result"),
     path("generate-code", GenerateQRCode, name="generateQRCode"),
-    path('display-qr-image', DisplayQRImage, name="displayQRImage")
+    path('display-qr-image', DisplayQRImage, name="displayQRImage"),
+    path("live-scan-qr-code", LiveScan, name="liveScan")
 ]
